50b5617fea1a298aeb4058a289e11a86e4709514
Luciano Porto <luciano.bapo@gmail.com>
2017-05-16 00:01:59 -0300

    automatic-commit-alias

--- src/v1/Entities/BaseEloquent.php
+++ src/v1/Entities/BaseEloquent.php
@@ -2,6 +2,7 @@
 
 namespace ErpNET\Models\v1\Entities;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Prettus\Repository\Contracts\Presentable;
@@ -35,9 +36,62 @@ abstract class BaseEloquent extends Model
         parent::__construct($attributes);
     }
 
-    protected function getDateFormat()
+    /**
+     * Return a timestamp as DateTime object.
+     *
+     * @param  mixed  $value
+     * @return \Carbon\Carbon
+     */
+    protected function asDateTime($value)
     {
-        return 'Y-m-d H:i:se';
+        // If this value is already a Carbon instance, we shall just return it as is.
+        // This prevents us having to re-instantiate a Carbon instance when we know
+        // it already is one, which wouldn't be fulfilled by the DateTime check.
+        if ($value instanceof \Carbon\Carbon) {
+            return $value;
+        }
+
+        // If the value is already a DateTime instance, we will just skip the rest of
+        // these checks since they will be a waste of time, and hinder performance
+        // when checking the field. We will just return the DateTime right away.
+        if ($value instanceof DateTimeInterface) {
+            return new \Carbon\Carbon(
+                $value->format('Y-m-d H:i:s.u'), $value->getTimezone()
+            );
+        }
+
+        // If this value is an integer, we will assume it is a UNIX timestamp's value
+        // and format a Carbon object from this timestamp. This allows flexibility
+        // when defining your date fields as they might be UNIX timestamps here.
+        if (is_numeric($value)) {
+            return \Carbon\Carbon::createFromTimestamp($value);
+        }
+
+        // If the value is in simply year, month, day format, we will instantiate the
+        // Carbon instances from that format. Again, this provides for simple date
+        // fields on the database, while still supporting Carbonized conversion.
+        if ($this->isStandardDateFormat($value)) {
+            return \Carbon\Carbon::createFromFormat('Y-m-d', $value)->startOfDay();
+        }
+
+        // Finally, we will just assume this date is in the format used by default on
+        // the database connection and use that format to create the Carbon object
+        // that is returned back out to the developers after we convert it here.
+
+//        return \Carbon\Carbon::createFromFormat(
+//            $this->getDateFormat(), $value
+//        );
+
+        try{
+            return \Carbon\Carbon::createFromFormat(
+                'Y-m-d H:i:s', $value
+            );
+        }
+        catch(\Exception $e){
+            return \Carbon\Carbon::createFromFormat(
+                'Y-m-d H:i:se', $value
+            );
+        }
     }
 
     /**
